5982468e7fdf8e000303a97b6c871837305d3b34
David Reina <davidereina@hotmail.com>
2023-10-06 09:45:44 -0500

    Simple program that print out in the console the even numbers of the given matrix

---
+++ EvenNumbersMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EvenNumbersMatrix
+{
+    internal class Program
+    {
+
+        static int[,] matrix =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] % 2 == 0)
+                    {
+                        Console.WriteLine(matrix[i,j] + " is an even number");
+                    }
+                }
+            }
+        }
+    }
+}
